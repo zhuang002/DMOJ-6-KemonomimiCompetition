d128fca551da0db79ed770a842cbbf0901f1c29a
zhuang002 <zhuang001@gmail.com>
2018-07-03 13:55:31 -0400

    using class

--- src/kemonomimicompetition/KemonomimiCompetition.java
+++ src/kemonomimicompetition/KemonomimiCompetition.java
@@ -14,39 +14,68 @@ import java.util.Scanner;
 public class KemonomimiCompetition {
     static Scanner sc=new Scanner(System.in);
     static int[] c=new int[4];
-    static int[] timeNeeded;
     static int elapsedTime=0;
-    static int[] pi;
+    static Record[] records;
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         int n=sc.nextInt();
-        timeNeeded=new int[n];
-        pi=new int[n];
+        
         for (int i=0;i<c.length;i++)
             c[i]=sc.nextInt();
         
+        records=new Record[n];
         for (int i=0;i<n;i++) {
-            calculateTimeNeeded(i);
+            records[i]=new Record();
+            records[i].read(sc);
+            
         }
+        
         for (int i=0;i<n;i++) {
-            if (timeNeeded[i]<=180-elapsedTime)
-                System.out.println(10-pi[i]);
-            else 
-                System.out.println("The kemonomimi are too cute!");
+            if (elapsedTime<records[i].si)
+                elapsedTime=records[i].si;
         }
+        
+        for (int i=0;i<n;i++) {
+            records[i].calculateAdditionalScore(c,elapsedTime);
+        }
+        
+        for (int i=0;i<n;i++) {
+            records[i].printAdditionalScore();
+        }
+    }
+}
+
+class Record {
+    int i;
+    int pi;
+    int si;
+    int ti;
+    private int additionalScore;
+
+    void calculateAdditionalScore(int[] c,int elapsedTime) {
+        int timeNeeded=c[this.i]*this.ti;
+        if (timeNeeded<=180-elapsedTime)
+            this.additionalScore=10-this.pi;
+        else
+            this.additionalScore=-1;
     }
 
-    private static void calculateTimeNeeded(int index) {
-        int i=sc.nextInt();
-        pi[index]=sc.nextInt();
-        int si=sc.nextInt();
-        int ti=sc.nextInt();
-        if (elapsedTime<si)
-            elapsedTime=si;
-        timeNeeded[index]=ti*c[i-1];
+    void printAdditionalScore() {
+        if (this.additionalScore>=0) {
+            System.out.println(this.additionalScore);
+        } else
+            System.out.println("The kemonomimi are too cute!");
     }
-    
+
+    void read(Scanner sc) {
+        this.i=sc.nextInt()-1;
+        this.pi=sc.nextInt();
+        this.si=sc.nextInt();
+        this.ti=sc.nextInt();
+    }
 }
